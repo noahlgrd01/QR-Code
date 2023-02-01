057321451dd516fbe7f6f738663d77dfa16df934
BigBossNoahL <63643071+BigBossNoahL@users.noreply.github.com>
2023-02-01 19:19:44 +0100

    Update qrcode.py
    Added possibility to enter .png name to find easier your QR Code

--- qrcode.py
+++ qrcode.py
@@ -3,10 +3,10 @@ import png
 from pyqrcode import QRCode
 
 link = input("Enter QR Code content : ")
+name = input("Enter QR Code name : ") or qrcode
 
 qr = pyqrcode.create(link)
 
-qr.svg("qrcode.svg", scale = 8)
-qr.png("qrcode.png", scale = 6)
+qr.png(name + ".png", scale = 6)
 
-print("QR Code has been generated ! Find the generated QR Code in qrcode.png")
+print("QR Code has been generated ! Find the generated QR Code in", name, ".png")
