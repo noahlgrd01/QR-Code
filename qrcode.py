7a9b97939a05170737f2316b010a53505467712b
NoahLgrd <63643071+noahlgrd01@users.noreply.github.com>
2023-10-21 13:42:32 +0200

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -9,15 +9,14 @@ while True:
 
     check = input("Generate QRCode ? Y/N ")
     if check == "Y" or check == "y":
-        link = input("Enter QR Code content : ")
-        name = input("Enter QR Code name : ") or "qrcode"
+        link = input("Enter QR Code content (could be a link, name etc) : ")
+        name = input("Enter QR Code name (or leave blank) : ") or "qrcode"
 
         qr = pyqrcode.create(link)
 
         qr.png(f"{name or 'qrcode'}_{i+1}.png", scale = 6)
 
-        print("QR Code has been generated ! Find the generated QR Code as .png file")
-        print("  ")
+        print("QR Code has been generated ! Find the generated QR Code as .png file\n")
         i +=1
 
     elif check == "N" or check == "n":
